61ad344527cf22328c42a1779f7741c76d788aee
Simon <SimonNgj@users.noreply.github.com>
2019-07-09 23:02:35 +1000

    Create PitchRoll.ino

---
+++ ArduinoIMU/PitchRoll.ino
@@ -0,0 +1,35 @@
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+MPU6050 accelgyro;
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+
+void setup() {
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    Serial.begin(38400);
+    accelgyro.initialize();
+}
+
+void loop() {
+
+    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+        Serial.print("a/g:\t");
+        Serial.print(ax); Serial.print("\t");
+        Serial.print(ay); Serial.print("\t");
+        Serial.print(az); Serial.print("\t");
+        Serial.print(gx); Serial.print("\t");
+        Serial.print(gy); Serial.print("\t");
+        Serial.println(gz);
+}
