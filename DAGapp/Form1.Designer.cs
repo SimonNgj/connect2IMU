fb7dcbcc6322b91fab5658cbaa95cedfdd4b2074
Simon <SimonNgj@users.noreply.github.com>
2019-09-28 15:41:33 +1000

    Add files via upload

---
+++ DAGapp/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace DAGapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.BtnConnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCom = new System.Windows.Forms.TextBox();
+            this.lbCOM = new System.Windows.Forms.Label();
+            this.txtBaud = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.LegendStyle = System.Windows.Forms.DataVisualization.Charting.LegendStyle.Column;
+            legend3.Name = "Legend1";
+            legend3.Position.Auto = false;
+            legend3.Position.Height = 9.026128F;
+            legend3.Position.Width = 15.875F;
+            legend3.Position.X = 81.125F;
+            legend3.Position.Y = 3F;
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(140, 12);
+            this.chart1.Name = "chart1";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series5.Color = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            series5.Legend = "Legend1";
+            series5.Name = "Data1";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series6.Color = System.Drawing.Color.Red;
+            series6.Legend = "Legend1";
+            series6.Name = "Data1 (Filtered)";
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(801, 422);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // BtnConnect
+            // 
+            this.BtnConnect.Location = new System.Drawing.Point(27, 109);
+            this.BtnConnect.Name = "BtnConnect";
+            this.BtnConnect.Size = new System.Drawing.Size(75, 23);
+            this.BtnConnect.TabIndex = 5;
+            this.BtnConnect.Text = "Connect";
+            this.BtnConnect.UseVisualStyleBackColor = true;
+            this.BtnConnect.Click += new System.EventHandler(this.BtnConnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Baud Rate";
+            // 
+            // txtCom
+            // 
+            this.txtCom.Location = new System.Drawing.Point(69, 30);
+            this.txtCom.Name = "txtCom";
+            this.txtCom.Size = new System.Drawing.Size(61, 20);
+            this.txtCom.TabIndex = 2;
+            this.txtCom.Text = "COM8";
+            this.txtCom.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lbCOM
+            // 
+            this.lbCOM.AutoSize = true;
+            this.lbCOM.Location = new System.Drawing.Point(5, 33);
+            this.lbCOM.Name = "lbCOM";
+            this.lbCOM.Size = new System.Drawing.Size(52, 13);
+            this.lbCOM.TabIndex = 9;
+            this.lbCOM.Text = "COM port";
+            // 
+            // txtBaud
+            // 
+            this.txtBaud.Location = new System.Drawing.Point(69, 62);
+            this.txtBaud.Name = "txtBaud";
+            this.txtBaud.Size = new System.Drawing.Size(61, 20);
+            this.txtBaud.TabIndex = 10;
+            this.txtBaud.Text = "115200";
+            this.txtBaud.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 153);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(118, 277);
+            this.listBox1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(953, 447);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtBaud);
+            this.Controls.Add(this.lbCOM);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnConnect);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.txtCom);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Data Acquisition ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button BtnConnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCom;
+        private System.Windows.Forms.Label lbCOM;
+        private System.Windows.Forms.TextBox txtBaud;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ DAGapp/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace DAGapp
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort sensport;
+        private string data;
+        double rt = 0;  
+        double k = 0;
+        Boolean i = false;
+        double ratio = 0.5;
+
+        // For Kalman filter
+        //double measurement_sigma = 1.0;
+        //double motion_sigma = 1.0;
+        //double mu = 0;
+        //double sigma = 1;
+
+        LowpassFilter lowpass1 = new LowpassFilter();
+        //KalmanFilters.KalmanFilter1D kal1D;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+            //kal1D = new KalmanFilters.KalmanFilter1D(mu, sigma, measurement_sigma, motion_sigma);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            rt = rt + 0.1;
+        }
+
+        private void BtnConnect_Click(object sender, EventArgs e)
+        {
+            if (BtnConnect.Text == "Connect")
+            {
+                BtnConnect.Text = "Close";
+
+                sensport = new SerialPort();
+                sensport.PortName = txtCom.Text;
+                sensport.BaudRate = int.Parse(txtBaud.Text);
+                sensport.Parity = Parity.None;
+                sensport.DataBits = 8;
+                sensport.StopBits = StopBits.One;
+                sensport.Handshake = Handshake.None;
+                sensport.DataReceived += sensport_DataReceived;
+
+                try
+                {
+                    sensport.Open();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error");
+                }
+            }
+            else
+            {
+                BtnConnect.Text = "Connect";
+                sensport.Close();
+            }
+        }
+
+        void sensport_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            k++;
+
+            if (i == false)
+            {
+                rt = 0;
+                i = true;
+            }
+
+            data = sensport.ReadLine();
+            listBox1.Items.Add(data);
+
+            if ((data == "") || k < 5)
+            { }
+            else
+            {
+                String[] datalist = data.Split(',');
+                int length = datalist.Length;
+                if (length != 2)
+                { }
+                else
+                {
+                    string data1 = datalist[0];
+                    //string data2 = datalist[1];
+                    this.chart1.Series["Data1"].Points.AddXY(rt, data1);
+                    
+                   //double data1b = lowpass1.firstOrder_lowpassFilter(double.Parse(data1), ratio);
+                   double data1b = lowpass1.secondOrder_lowpassFilter(double.Parse(data1), ratio);
+                   //double data1b = lowpass1.thirdOrder_lowpassFilter(double.Parse(data1), ratio);
+
+                    //kal1D.Update(rt);
+                    //kal1D.Predict(double.Parse(data1));
+                    //label2.Text = kal1D.State.ToString();
+
+                    this.chart1.Series["Data1 (Filtered)"].Points.AddXY(rt, data1b);
+                }
+            }
+        }
+    }
+}
